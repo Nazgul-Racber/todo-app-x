594f76a678e76f8dabc853ccb3a32e21bbc4b1dd
email <racbernazgul@gmail.com>
2023-06-16 01:56:18 +0300

    tasks

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const input = document.getElementById("input");
 const btn = document.getElementById("btn");
 const ul = document.querySelector("ul");
 
+let tasks = [];
 
 btn.addEventListener("click", () => {
-    if (!input.value) {
-        alert("plase enter your todo...")
-    } else {
-        const task = {
-            id: new Date().getTime(),
-            text:input.value,
-            flag:false,
-        }
-        console.log(task.id);
-        // console.log(new Date().getTime());
-    }
-})
+  if (!input.value) {
+    alert("plase enter your todo...");
+  } else {
+    const task = {
+      id: new Date().getTime(), //Date.now()
+      text: input.value,
+      flag: false,
+    };
+    // console.log(task.id);
+    // console.log(new Date().getTime());
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    // localStorage.clear(); //?local'i temizlemek için.
+  }
+  console.log(tasks);
+});
