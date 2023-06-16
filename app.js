6b53251c6d112e7b3e6bf3e7e23cb6fa099dfbfe
email <racbernazgul@gmail.com>
2023-06-16 17:35:50 +0300

    toggle

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const btn = document.getElementById("btn");
 const ul = document.querySelector("ul");
 
 //? function
-function domaYaz({ id, text, flag }) {
+function domaYaz({ id, text}) {
   //const { id, text, flag } = task;
-  ul.innerHTML += ` <li id=${id} class=${flag? "checked":""}>
-  <i class="fa fa-check"></i><span>${text}</span><i class="fa fa-trash"></i></li>`;
+  ul.innerHTML += ` <li id=${id} class=''><i class="fa fa-check"></i><span>${text}</span><i class="fa fa-trash"></i></li>`;
 }
 
 let tasks = [];
@@ -19,12 +18,12 @@ btn.addEventListener("click", () => {
     const task = {
       id: new Date().getTime(), //Date.now()
       text: input.value,
-      flag: false,
+      
     };
     // console.log(task.id);
     // console.log(new Date().getTime());
     tasks.push(task);
-    localStorage.setItem('tasks',JSON.stringify(tasks));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
     // localStorage.clear(); //?local'i temizlemek için.
 
     domaYaz(task);
@@ -35,27 +34,36 @@ btn.addEventListener("click", () => {
 });
 
 //enter
-window.addEventListener("keydown", (e) => {
+  window.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-      btn.click()
+    btn.click();
   }
-})
+});
 
-ul.addEventListener("click", (e) => {
+  ul.addEventListener("click", (e) => {
   if (e.target.classList.contains("fa-trash")) {
     const id = e.target.parentElement.id;
-    if (e.target.classList.contains("fa-trash")) 
-      tasks = tasks.filter((task)=> task.id != id) //? çöp kutusuna tıklanan li'nin arrayden atılma işlemi.
-      localStorage.setItem('tasks', JSON.stringify(tasks))
-      e.target.parentElement.remove();
+    if (e.target.classList.contains("fa-trash"))
+      tasks = tasks.filter((task) => task.id != id); //? çöp kutusuna tıklanan li'nin arrayden atılma işlemi.
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    e.target.parentElement.remove();
   }
 
-  if (e.target.classList.contains("fa-checked"))
-})
+  // if (e.target.classList.contains("checked")) {
+  //   e.target.parentElement.classList.remove("checked");
+  // } else {
+  //   //?ilgili li elementinde checked adında bir class yoksa ekle
+  //   e.target.parentElement.classList.add("checked");
+  // }
+    
+    if (e.target.parentElement.classList.toggle("checked"))
+    { }    
+    
+});
 
 window.addEventListener("load", () => {
   tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   tasks.forEach((task) => {
     domaYaz(task);
-  })
-})
+  });
+});
